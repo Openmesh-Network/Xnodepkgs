108501ee713b47db8d342aeaede2951c5f20cdfc
dowdt <tomas.dowd@openmesh.network>
2024-07-26 11:18:28 +1000

    Undid dummy change and updated admin service.

--- pkgs/openmesh/xnode/admin/default.nix
+++ pkgs/openmesh/xnode/admin/default.nix
@@ -2,14 +2,14 @@
 
 pkgs.python3Packages.buildPythonPackage rec {
   pname = "xnode-admin";
-  version = "0.0.1";
+  version = "0.0.0";
   format = "pyproject";
 
   src = pkgs.fetchFromGitHub {
     owner = "Openmesh-Network";
     repo = pname;
-    rev = "31a10d1f951c956300342256a29e79aff51699ef";
-    sha256 = "0njbgibf788fpfsxsv1dvwp01yrf9k696rd4p4ppyxvz6xac1cxz";
+    rev = "4331cb33a6994c73f12d1a4721a53cd73d49167e";
+    sha256 = "0gvshdvsxay32yd5njkxf5yhi77pkdpr0zm25mh14zlhc4i7992m";
   };
 
   nativeBuildInputs = [
